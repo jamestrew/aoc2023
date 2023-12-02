2bf05900f67925163340db3bb4e326a563a7c098
James Trew <j.trew10@gmail.com>
2023-12-01 21:22:02 -0500

    day1 quarter assed

---
+++ days/day1/src/main.rs
@@ -0,0 +1,53 @@
+fn main() {
+    let input = include_str!("../../../data/day1");
+    println!("PART 1: {}", part1(input));
+    println!("PART 2: {}", part2(input));
+}
+
+fn part1(input: &str) -> usize {
+    input
+        .lines()
+        .map(|line| {
+            let v = line
+                .chars()
+                .filter(|char| char.is_numeric())
+                .collect::<Vec<_>>();
+
+            let first = v.first().unwrap();
+            let last = v.last().unwrap();
+            let num = format!("{first}{last}");
+            num.parse::<usize>().unwrap()
+        })
+        .sum()
+}
+
+fn part2(input: &str) -> usize {
+    todo!("nah")
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn part1_sample() {
+        let input = "1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet";
+        assert_eq!(part1(input), 142);
+    }
+
+    #[test]
+    fn part2_sample() {
+        let input = "two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen";
+
+        assert_eq!(part2(input), 281);
+    }
+}
